cbb85ea1e39b8aec5a038442be284e2ae60fe106
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-09-22 15:20:46 +0200

    Add Client React project

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import NavBar from "./components/navbar";
+import Card from "./components/card";
+
+function App() {
+    return (
+        <React.Fragment>
+            <NavBar/>
+            <Card/>
+        </React.Fragment>
+    );
+}
+
+export default App;
