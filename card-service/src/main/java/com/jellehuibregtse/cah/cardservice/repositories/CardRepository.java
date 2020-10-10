65ed8cd3d7a7552ac0dba071ac0e846e08083af6
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-10-10 13:24:45 +0200

    Update Card repository

--- card-service/src/main/java/com/jellehuibregtse/cah/cardservice/repositories/CardRepository.java
+++ card-service/src/main/java/com/jellehuibregtse/cah/cardservice/repositories/CardRepository.java
@@ -4,10 +4,5 @@ import com.jellehuibregtse.cah.cardservice.models.Card;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface CardRepository extends CrudRepository<Card, Long> {
-    @Override
-    List<Card> findAll();
-}
+public interface CardRepository extends CrudRepository<Card, Long> {}
