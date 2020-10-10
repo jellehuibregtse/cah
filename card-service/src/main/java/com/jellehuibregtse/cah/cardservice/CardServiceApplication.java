aad76ddc62c5fbf7ecd01cb1fa0ec6a0bd87198f
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-10-10 13:51:39 +0200

    Update CardServiceApplication.java

--- card-service/src/main/java/com/jellehuibregtse/cah/cardservice/CardServiceApplication.java
+++ card-service/src/main/java/com/jellehuibregtse/cah/cardservice/CardServiceApplication.java
@@ -1,10 +1,8 @@
 package com.jellehuibregtse.cah.cardservice;
 
-import com.jellehuibregtse.cah.cardservice.repositories.CardRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -15,5 +13,4 @@ public class CardServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CardServiceApplication.class, args);
     }
-
 }
