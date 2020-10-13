bc4078b7d8e4af6542e01c0fffc5bb78247b290f
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-10-13 11:47:48 +0200

    Add @RunWith(SpringRunner.class)

--- card-service/src/test/java/com/jellehuibregtse/cah/cardservice/CardControllerTests.java
+++ card-service/src/test/java/com/jellehuibregtse/cah/cardservice/CardControllerTests.java
@@ -7,11 +7,13 @@ import com.jellehuibregtse.cah.cardservice.models.CardType;
 import com.jellehuibregtse.cah.cardservice.repositories.CardRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -23,6 +25,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
