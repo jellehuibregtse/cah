d42de8d7679c6b605a218cd996150857a9952d5d
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-10-13 11:29:04 +0200

    Add test as active profile to application test

--- card-service/src/test/java/com/jellehuibregtse/cah/cardservice/CardServiceApplicationTests.java
+++ card-service/src/test/java/com/jellehuibregtse/cah/cardservice/CardServiceApplicationTests.java
@@ -5,8 +5,10 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("tests")
 public class CardServiceApplicationTests {
 
     @Autowired
