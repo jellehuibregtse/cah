96f92e7aef4dd34d22fdf4c4fc3bcb43bd3ea2a5
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-10-13 11:25:53 +0200

    Fix CardServiceApplicationTests

--- card-service/src/test/java/com/jellehuibregtse/cah/cardservice/CardServiceApplicationTests.java
+++ card-service/src/test/java/com/jellehuibregtse/cah/cardservice/CardServiceApplicationTests.java
@@ -1,11 +1,10 @@
 package com.jellehuibregtse.cah.cardservice;
 
 import com.jellehuibregtse.cah.cardservice.controllers.CardController;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class CardServiceApplicationTests {
@@ -15,6 +14,6 @@ public class CardServiceApplicationTests {
 
     @Test
     public void contextLoads() {
-        assertThat(cardController).isNotNull();
+        Assert.assertNotNull(cardController);
     }
 }
