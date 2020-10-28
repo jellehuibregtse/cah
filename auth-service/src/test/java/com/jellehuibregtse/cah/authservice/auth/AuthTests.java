37e32cc4e166a4e7cf6f1870998a50e5c076d625
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-10-28 10:04:31 +0100

    Add initial auth test

--- auth-service/src/test/java/com/jellehuibregtse/cah/authservice/auth/AuthTests.java
+++ auth-service/src/test/java/com/jellehuibregtse/cah/authservice/auth/AuthTests.java
@@ -1,3 +1,27 @@
 package com.jellehuibregtse.cah.authservice.auth;
 
-public class AuthTests {}
+import com.jellehuibregtse.cah.authservice.jwt.JwtConfig;
+import com.jellehuibregtse.cah.authservice.service.JwtTokenService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class AuthTests {
+
+    @Autowired
+    private JwtTokenService jwtTokenService;
+
+    @Autowired
+    private JwtConfig jwtConfig;
+
+    @Test
+    public void generateToken_returnsString() {
+        var token = jwtTokenService.generateToken("subject");
+
+        assertNotNull(token);
+    }
+}
