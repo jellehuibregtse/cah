0bdecb63ca0c2bbd045992af4007d4b6d8cfaedf
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-12-01 13:57:11 +0100

    Add user controller
    You can now register and login users.

---
+++ auth-service/src/main/java/com/jellehuibregtse/cah/authservice/controller/UserController.java
@@ -0,0 +1,109 @@
+package com.jellehuibregtse.cah.authservice.controller;
+
+import com.jellehuibregtse.cah.authservice.model.ApplicationUser;
+import com.jellehuibregtse.cah.authservice.repository.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * The controller that handles all the mappings for the user service.
+ *
+ * @author Jelle Huibregtse
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserRepository applicationUserRepository;
+
+    @GetMapping("/logged-in")
+    public ResponseEntity<Boolean> loggedIn() {
+        return ResponseEntity.ok(true);
+    }
+
+    @Autowired
+    public UserController(PasswordEncoder passwordEncoder, ApplicationUserRepository applicationUserRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.applicationUserRepository = applicationUserRepository;
+    }
+
+    /**
+     * Check if email is taken.
+     *
+     * @param email that needs to be found.
+     * @return <code>ResponseEntity</code> with a message and HTTP status OK.
+     */
+    @GetMapping
+    public ResponseEntity<Boolean> emailTaken(@RequestParam String email) {
+        return ResponseEntity.ok(applicationUserRepository.findByUsername(email).isPresent());
+    }
+
+    /**
+     * Create a user.
+     *
+     * @param user that needs to created
+     * @return <code>ResponseEntity</code> with a message and HTTP status OK.
+     */
+    @PostMapping
+    public ResponseEntity<String> createApplicationUser(@RequestBody ApplicationUser user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        applicationUserRepository.save(user);
+
+        return ResponseEntity.ok(String.format("User with email: %s has been successfully created!",
+                                               user.getUsername()));
+    }
+
+    /**
+     * Update a single user.
+     *
+     * @param user that needs to be updated
+     * @return message and HTTP status OK.
+     */
+    @PutMapping
+    public ResponseEntity<String> updateApplicationUser(@RequestBody ApplicationUser user) {
+        var principal = getPrincipal();
+        var updatedApplicationUser = applicationUserRepository.findByUsername(principal)
+                                                              .orElseThrow(() -> new ResourceNotFoundException(
+                                                                      "Not found."));
+
+        updatedApplicationUser.setPassword(user.getPassword());
+        updatedApplicationUser.setUsername(user.getUsername());
+
+        applicationUserRepository.save(updatedApplicationUser);
+
+        return ResponseEntity.ok(String.format("User with id: %d has been successfully updated!",
+                                               updatedApplicationUser.getId()));
+    }
+
+    /**
+     * Delete a single user.
+     *
+     * @return <code>ResponseEntity</code> with a message and HTTP status OK.
+     */
+    @DeleteMapping
+    public ResponseEntity<String> deleteApplicationUser() {
+        var principal = getPrincipal();
+
+        var user = applicationUserRepository.findByUsername(principal)
+                                            .orElseThrow(() -> new ResourceNotFoundException("Not found."));
+
+        applicationUserRepository.delete(user);
+
+        return ResponseEntity.ok(String.format("User with id: %d has been successfully deleted!", user.getId()));
+    }
+
+    /**
+     * Gets the principal (email) of the currently logged in user.
+     *
+     * @return email.
+     */
+    private String getPrincipal() {
+        return (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+}
